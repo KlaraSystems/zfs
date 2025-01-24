f550eb7b878c10c16c6f791ee31bee646dffd4b2
Paul Dagnelie <paul.dagnelie@klarasystems.com>
2025-01-23 16:26:09 -0800

    Implement dynamic gang header sizes
    ZFS gang block headers are currently fixed at 512 bytes. This is
    increasingly wasteful in the era of larger disk sector sizes. This PR
    allows any size allocation to work as a gang header. It also contains
    supporting changes to ZDB to make gang headers easier to work with.
    
    Sponsored-by: Klara, Inc.
    Sponsored-by: Wasabi Technology, Inc.
    Reviewed-by: Alexander Motin <mav@FreeBSD.org>
    Reviewed-by: Tony Hutter <hutter2@llnl.gov>
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Reviewed-by: Rob Norris <rob.norris@klarasystems.com>
    Reviewed-by: Allan Jude <allan@klarasystems.com>
    Signed-off-by: Paul Dagnelie <paul.dagnelie@klarasystems.com>
    Closes #17004

--- module/zcommon/zfeature_common.c
+++ module/zcommon/zfeature_common.c
@@ -798,6 +798,12 @@ zpool_feature_init(void)
 		    slack_deps, sfeatures);
 	}
 
+	zfeature_register(SPA_FEATURE_DYNAMIC_GANG_HEADER,
+	    "com.klarasystems:dynamic_gang_header", "dynamic_gang_header",
+	    "Support for dynamically sized gang headers",
+	    ZFEATURE_FLAG_MOS | ZFEATURE_FLAG_NO_UPGRADE,
+	    ZFEATURE_TYPE_BOOLEAN, NULL, sfeatures);
+
 	zfs_mod_list_supported_free(sfeatures);
 }
 
