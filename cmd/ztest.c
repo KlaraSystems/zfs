5dad29320ab1db25b2a4b24e263599e05fee8d5d
Igor Ostapenko <igor.ostapenko@klarasystems.com>
2025-09-19 08:11:03 +0000

    hfe: ztest: Add support of ztest_device_removal()
    Sponsored-by: Klara, Inc.
    Sponsored-by: Wasabi Technology, Inc.
    Signed-off-by: Igor Ostapenko <igor.ostapenko@klarasystems.com>

--- cmd/ztest.c
+++ cmd/ztest.c
@@ -4448,7 +4448,8 @@ ztest_device_removal(ztest_ds_t *zd, uint64_t id)
 	 */
 	error = spa_scan(spa, POOL_SCAN_SCRUB);
 	if (error == 0) {
-		while (dsl_scan_scrubbing(spa_get_dsl(spa)))
+		while (dsl_scan_scrubbing(spa_get_dsl(spa)) &&
+		    !ZTEST_HFE_ACTIVE())
 			txg_wait_synced(spa_get_dsl(spa), 0);
 	}
 
